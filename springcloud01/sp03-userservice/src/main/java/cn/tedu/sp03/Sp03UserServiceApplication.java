2c572874681c995b668c899eeefeedee761004e4
Yzw <1741361440@qq.com>
2021-08-12 16:49:21 +0800

    commons 通用项目

---
+++ springcloud01/sp03-userservice/src/main/java/cn/tedu/sp03/Sp03UserServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp03UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp03UserServiceApplication.class, args);
+    }
+
+}
