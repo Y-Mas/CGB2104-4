c49ccc90ca3c30a548a263044d41d1f33e979fd9
Yzw <1741361440@qq.com>
2021-08-13 09:09:23 +0800

    commons 通用项目

--- springcloud01/sp05-eureka/src/main/java/cn/tedu/sp05/Sp05EurekaApplication.java
+++ springcloud01/sp05-eureka/src/main/java/cn/tedu/sp05/Sp05EurekaApplication.java
@@ -2,8 +2,10 @@ package cn.tedu.sp05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Sp05EurekaApplication {
 
     public static void main(String[] args) {
