c71f27b39716156e1f5b33b481dccb0e1a65f079
Yzw <1741361440@qq.com>
2021-08-13 14:49:18 +0800

    commons 通用项目

---
+++ springcloud01/sp04-orderservice/src/main/java/cn/tedu/sp04/feign/UserClient.java
@@ -0,0 +1,16 @@
+package cn.tedu.sp04.feign;
+
+import cn.tedu.sp01.pojo.User;
+import cn.tedu.web.util.JsonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "user-service")
+public interface UserClient {
+    @GetMapping("/{userId}")
+    JsonResult<User> getUser(@PathVariable("userId") Integer userId);
+    @GetMapping("/{userId}/score")
+    JsonResult<?> addScore(@PathVariable Integer userId,@RequestParam("score") Integer score);
+}
--- springcloud01/sp04-orderservice/src/main/java/cn/tedu/sp04/service/OrderServiceImpl.java
+++ springcloud01/sp04-orderservice/src/main/java/cn/tedu/sp04/service/OrderServiceImpl.java
@@ -1,30 +1,47 @@
 package cn.tedu.sp04.service;
 
+import cn.tedu.sp01.pojo.Item;
 import cn.tedu.sp01.pojo.Order;
+import cn.tedu.sp01.pojo.User;
 import cn.tedu.sp01.service.OrderService;
+import cn.tedu.sp04.feign.ItemClient;
+import cn.tedu.sp04.feign.UserClient;
+import cn.tedu.web.util.JsonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private ItemClient itemClient;
+    @Autowired
+    private UserClient userClient;
+
     @Override
     public Order getOrder(String orderId) {
         log.info("获取订单,orderId="+orderId);
         //TODO: 远程调用商品,获取商品列表
+        JsonResult<List<Item>> items = itemClient.getItems(orderId);
         //TODO: 远程调用用户,获取用户列表
+        JsonResult<User> user = userClient.getUser(5);
+
         Order order = new Order();
         order.setId(orderId);
-//        order.setItems(商品列表);
-//        order.setUser(用户);
+        order.setItems(items.getData());
+        order.setUser(user.getData());
         return order;
     }
 
     @Override
     public void addOrder(Order order) {
         log.info("添加订单,order="+order);
-
         //TODO: 远程调用商品,减少商品库存
+        itemClient.decreaseNumber(order.getItems());
         //TODO: 远程调用用户,增加积分
+        userClient.addScore(order.getUser().getId(),500);
     }
 }
