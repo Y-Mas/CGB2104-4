ade20dfe71883a3f665906d6cee7dd36ea9dc1e0
Yzw <1741361440@qq.com>
2021-08-24 15:45:44 +0800

    id生成器

--- seata-at/order-parent/order/src/main/java/cn/tedu/order/service/OrderServiceImpl.java
+++ seata-at/order-parent/order/src/main/java/cn/tedu/order/service/OrderServiceImpl.java
@@ -14,8 +14,14 @@ public class OrderServiceImpl implements OrderService{
 
     @Override
     public void create(Order order) {
+        //TODO: 远程调用ID发号器,获取订单id
+
+        //临时随机产生id
         Long orderId = Long.valueOf(new Random().nextInt(Integer.MAX_VALUE));
         order.setId(orderId);
         orderMapper.create(order);
+
+        //TODO: 远程调用库存,减少库存
+        //TODO: 远程调用账户,扣减账户
     }
 }
