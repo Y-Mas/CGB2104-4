fae9b02eceb6fdf46bc695c245dcd18c2bc56760
Yzw <1741361440@qq.com>
2021-08-24 16:56:40 +0800

    分布式事务

--- seata-at/order-parent/order/src/main/java/cn/tedu/order/OrderApplication.java
+++ seata-at/order-parent/order/src/main/java/cn/tedu/order/OrderApplication.java
@@ -3,9 +3,12 @@ package cn.tedu.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @MapperScan("cn.tedu.order.mapper")
+@EnableFeignClients
 public class OrderApplication {
 
     public static void main(String[] args) {
--- seata-at/order-parent/order/src/main/java/cn/tedu/order/service/OrderServiceImpl.java
+++ seata-at/order-parent/order/src/main/java/cn/tedu/order/service/OrderServiceImpl.java
@@ -1,6 +1,9 @@
 package cn.tedu.order.service;
 
 import cn.tedu.order.entity.Order;
+import cn.tedu.order.feign.AccountClient;
+import cn.tedu.order.feign.EasyIdClient;
+import cn.tedu.order.feign.StorageClient;
 import cn.tedu.order.mapper.OrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,17 +14,24 @@ import java.util.Random;
 public class OrderServiceImpl implements OrderService{
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private EasyIdClient easyIdClient;
+    @Autowired
+    private StorageClient storageClient;
+    @Autowired
+    private AccountClient accountClient;
+
 
     @Override
     public void create(Order order) {
         //TODO: 远程调用ID发号器,获取订单id
-
-        //临时随机产生id
-        Long orderId = Long.valueOf(new Random().nextInt(Integer.MAX_VALUE));
-        order.setId(orderId);
+        Long id = Long.valueOf(easyIdClient.nextId("order_business"));
+        order.setId(id);
         orderMapper.create(order);
 
         //TODO: 远程调用库存,减少库存
+        storageClient.decrease(order.getProductId(), order.getCount());
         //TODO: 远程调用账户,扣减账户
+        accountClient.decrease(order.getUserId(), order.getMoney());
     }
 }
